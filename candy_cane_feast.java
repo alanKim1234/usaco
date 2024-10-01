71ba69f8e88f0cc5299f568a55efa81ac4e98645
alanKim1234 <alan.dowan.kim@gmail.com>
2024-09-30 20:41:28 -0700

    Changes

--- candy_cane_feast.java
+++ candy_cane_feast.java
@@ -3,39 +3,38 @@ import java.util.*;
 
 public class candy_cane_feast {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("2.in"));
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String s1 = br.readLine();
-        int n = Integer.parseInt(st.nextToken()); //read in n
+        int n = Integer.parseInt(st.nextToken()); //read in n  
         int m = Integer.parseInt(st.nextToken()); //read in m
         
         st = new StringTokenizer(br.readLine());
-        String s2 = br.readLine();
         long[] mList = convert(n, st);//Cow heights
 
         long[] taken_values = new long[m]; 
 
-        st = new StringTokenizer(br.readLine());
-        br.mark(s1.length() + s2.length());
+        String candy_cane_heights = br.readLine();
+        String[] ccH = candy_cane_heights.split(" ");
 
-        for (long cow : mList) {
+        for (int j = 0; j < mList.length; j++) {
             for (int i = 0; i < m; i++) {
-                long current_candy = Long.parseLong(st.nextToken());
-                if (cow > current_candy && current_candy > taken_values[i]) {
-                    long inc = Math.min(cow, current_candy) - taken_values[i];
+                long current_candy = Long.parseLong(ccH[i]);
+                long inc = Math.min(mList[j], current_candy) - taken_values[i];
 
-                    if (inc > 0) {
-                        cow += inc;
-                        taken_values[i] = inc;
-                    }
+                if (inc > 0) {
+                    mList[j] += inc;
+                    taken_values[i] = inc;
                 }
+                
             }
-            br.reset();
-            System.out.println(cow);
+        }
+
+        for (int i = 0; i < mList.length; i++) {
+            System.out.println(mList[i]);
         }
     }
+
 
     public static long[] returnCandyHeights(int h) {
         long[] arr = new long[h];
