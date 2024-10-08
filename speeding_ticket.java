7e711e99bbeb1e92bfdb51a2e7cfbc48eda2bd53
Alan Kim <alan.dowan.kim@gmail.com>
2024-10-07 22:31:06 -0700

    c

--- speeding_ticket.java
+++ speeding_ticket.java
@@ -3,18 +3,53 @@ import java.util.*;
 
 public class speeding_ticket {
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new FileReader("speeding.in"));
+        BufferedReader br = new BufferedReader(new FileReader("test.in"));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        PrintWriter pw = new PrintWriter(new File("speeding.out"));
+        //PrintWriter pw = new PrintWriter(new File("speeding.out"));
         
         int m = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
         
-        int[][] segments = new int[n][2];
+        int[] r_segments = new int[100];
+        int[] segments = new int[100];
 
+        int start = 0;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            
+            int length = Integer.parseInt(st.nextToken());
+            int speed = Integer.parseInt(st.nextToken());
+
+            for (int j = start; j < length; j++) {
+                r_segments[j] = speed;
+            }
+
+            start += length;
         }
+
+        start = 0;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int length = Integer.parseInt(st.nextToken());
+            int speed = Integer.parseInt(st.nextToken());
+
+            for (int j = start; j < length; j++) {
+                segments[j] = speed;
+            }
+
+            start += length;
+        }
+
+        int max_diff = 0;
+        for (int i = 0; i < 100; i++) {
+            if (segments[i] > r_segments[i]) {
+                max_diff = Math.max(max_diff, segments[i] - r_segments[i]);
+            }
+        }
+
+        System.out.println(max_diff);
+        //pw.println(max_diff);
+        //pw.close();
+        br.close();
+
     }
 }
