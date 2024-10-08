8c53e16cdbc94115f6bf062da333882d248ed90c
Alan Kim <alan.dowan.kim@gmail.com>
2024-10-07 21:45:57 -0700

    changes

--- speeding_ticket.java
+++ speeding_ticket.java
@@ -5,6 +5,16 @@ public class speeding_ticket {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new FileReader("speeding.in"));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        PrintWriter pw = new PrintWriter(new File("speeding.out"));
         
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        
+        int[][] segments = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            
+        }
     }
 }
