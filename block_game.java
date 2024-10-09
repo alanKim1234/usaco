95a0513c7c5a5b109a4976970d26552c0b8cee8c
Alan Kim <alan.dowan.kim@gmail.com>
2024-10-08 19:15:08 -0700

    finished block

--- block_game.java
+++ block_game.java
@@ -2,5 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class block_game {
-    
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader("blocks.in"));
+        PrintWriter pw = new PrintWriter(new File("blocks.out"));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        
+        int[] alphabet = new int[26];
+
+        for (int i = 0; i < n; i++) {
+            int[] betainc = new int[26];
+            int[] alphainc = new int[26];
+            boolean[] flag = new boolean[26];
+            String s = br.readLine();
+            String[] words = s.split(" ");
+
+            for (String word : words) {
+                for (int k = 0; k < word.length(); k++) {
+                    int index = word.charAt(k) - 97;
+                    if (Character.isLetter(word.charAt(k)) && flag[index] == false) {
+                        ++alphainc[index];
+                    } else {
+                        ++betainc[index];
+                    }
+                }
+                for (int k = 0; k < word.length(); k++) {
+                    flag[word.charAt(k) - 97] = true;
+                }
+            }
+
+            for (int a = 0; a < 26; a++) {
+                alphabet[a] += Math.max(alphainc[a], betainc[a]); 
+            }
+        }
+
+
+        for (int i : alphabet) {
+            pw.println(i);
+        }
+
+        pw.close();
+        br.close();
+    }   
 }
