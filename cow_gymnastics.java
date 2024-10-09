f68f2253ab26f8194a76a3afa9ad594ca8b58505
Alan Kim <alan.dowan.kim@gmail.com>
2024-10-08 22:36:39 -0700

    Finished

--- cow_gymnastics.java
+++ cow_gymnastics.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class cow_gymnastics {
 
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new FileReader("test.in"));
-        PrintWriter pw = new PrintWriter(new File("test.out"));
+        BufferedReader br = new BufferedReader(new FileReader("gymnastics.in"));
+        PrintWriter pw = new PrintWriter(new File("gymnastics.out"));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int k = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
@@ -23,25 +23,39 @@ public class cow_gymnastics {
         ArrayList<Point> con_pairs = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (j != i) {
+            for (int j = i; j < n; j++) {
+                if (j != i && i < j) {
                     con_pairs.add(new Point(p[0][i], p[0][j]));
                 }
             }
         }
 
-
         for (int i = 1; i < k; i++) {
-            for (Point pair : con_pairs) {
-                if (Arrays.binarySearch(p[i], (int) pair.getX()) > 
-                    Arrays.binarySearch(p[i], (int) pair.getY())) {
-
-                    con_pairs.remove(pair);
+            Iterator<Point> it = con_pairs.iterator();
+            while (it.hasNext()) {
+                Point pair = it.next();
+                // 4 1 3 2
+                // (1, 2)
+                // 
+                if (returnIndex(p[i], (int) pair.getX()) > 
+                    returnIndex(p[i], (int) pair.getY())) {
+                    
+                    it.remove();
                 }
             }
         }
+
         pw.println(con_pairs.size());
         pw.close();
         br.close();
+    }
+
+
+    public static int returnIndex(int[] a, int n) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == n) return i;
+        }
+
+        return -1;
     }
 }
