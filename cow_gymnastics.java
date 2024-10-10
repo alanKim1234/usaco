ceb385f1f5e712b1c6e2e0856d8d76688a1886ea
Alan Kim <alan.dowan.kim@gmail.com>
2024-10-10 14:38:55 -0700

    commit

--- cow_gymnastics.java
+++ cow_gymnastics.java
@@ -5,49 +5,48 @@ import java.util.*;
 public class cow_gymnastics {
 
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new FileReader("gymnastics.in"));
-        PrintWriter pw = new PrintWriter(new File("gymnastics.out"));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int k = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-
-        int[][] p = new int[k][n];
-
-        for (int i = 0; i < k; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                p[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        ArrayList<Point> con_pairs = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (j != i && i < j) {
-                    con_pairs.add(new Point(p[0][i], p[0][j]));
+        try (BufferedReader br = new BufferedReader(new FileReader("gymnastics.in")); 
+            PrintWriter pw = new PrintWriter(new File("gymnastics.out"))) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            
+            int[][] p = new int[k][n];
+            
+            for (int i = 0; i < k; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) {
+                    p[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-        }
-
-        for (int i = 1; i < k; i++) {
-            Iterator<Point> it = con_pairs.iterator();
-            while (it.hasNext()) {
-                Point pair = it.next();
-                // 4 1 3 2
-                // (1, 2)
-                // 
-                if (returnIndex(p[i], (int) pair.getX()) > 
-                    returnIndex(p[i], (int) pair.getY())) {
-                    
-                    it.remove();
+            
+            ArrayList<Point> con_pairs = new ArrayList<>();
+            
+            for (int i = 0; i < n; i++) {
+                for (int j = i; j < n; j++) {
+                    if (j != i && i < j) {
+                        con_pairs.add(new Point(p[0][i], p[0][j]));
+                    }
                 }
             }
+            
+            for (int i = 1; i < k; i++) {
+                Iterator<Point> it = con_pairs.iterator();
+                while (it.hasNext()) {
+                    Point pair = it.next();
+                    // 4 1 3 2
+                    // (1, 2)
+                    //
+                    if (returnIndex(p[i], (int) pair.getX()) >
+                            returnIndex(p[i], (int) pair.getY())) {
+                        
+                        it.remove();
+                    }
+                }
+            }
+            
+            pw.println(con_pairs.size());
         }
-
-        pw.println(con_pairs.size());
-        pw.close();
-        br.close();
     }
 
 
